96b3a4fe94c47b62bca36d9af38bde1e77cbe76c
Edouard Roufay <roufaym@gmail.com>
2023-02-03 02:51:49 +0000

    Updated styling

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -8,7 +8,7 @@ const UI = () => {
     const [showCheapestOnly, setShowCheapestOnly] = useState(true)
   return (
     <div className='w-full'>
-        <div className='flex w-2/4 flex-col justify-center align-center mx-auto'>
+        <div className='flex lg:w-2/4 flex-col justify-center align-center mx-auto'>
           <div className='h-20 mx-5 mt-10 flex flex-cols justify-between items-center'>
             <Searchbox value={filterText} event={e => setFilterText(e.target.value)} />
             <Toggle isChecked={showCheapestOnly} event={()=> setShowCheapestOnly(!showCheapestOnly)}/>
